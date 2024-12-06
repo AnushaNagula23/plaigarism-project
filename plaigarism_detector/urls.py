c281f3644c4f559fb7f52fb34209f07dee3b85b2
saianiketh <aniketh@smartinterviews.in>
2024-12-06 12:23:59 +0530

    -m

--- plaigarism_detector/urls.py
+++ plaigarism_detector/urls.py
@@ -20,6 +20,9 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 
+
+
+
 @api_view(['GET', 'POST'])
 def plaigarismReport(request):
 	if request.method == 'GET':
